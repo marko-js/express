b8272301b0ed901ad149864c8cf2fec271f6f313
Dylan Piercey <dpiercey@ebay.com>
2021-02-23 21:12:01 -0700

    chore: update deps, switch to gh actions (#3)
    * chore: update deps, switch to gh actions
    
    * test: improve test coverage
    
    * chore: update linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,13 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:jest/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier/@typescript-eslint"
+    "plugin:prettier/recommended"
   ],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-use-before-define": "off"
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-explicit-any": "off"
   }
 };
--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -4,6 +4,8 @@ import express from "express";
 import fetch from "node-fetch";
 import markoMiddleware from "../index";
 import SimpleTemplate from "./fixtures/simple.marko";
+import DynamicTemplate from "./fixtures/dynamic.marko";
+import GlobalsTemplate from "./fixtures/globals.marko";
 import ErrorTemplate from "./fixtures/error.marko";
 
 test("Simple Template", async () => {
@@ -25,6 +27,45 @@ test("Simple Template", async () => {
   expect(html).toMatchInlineSnapshot(`"<div>Hello world</div>"`);
 });
 
+test("Dynamic Template", async () => {
+  const { res, html } = await fetchHtml(
+    express()
+      .use(markoMiddleware())
+      .use((req, res) => {
+        res.marko(DynamicTemplate, { name: "Dylan" });
+      })
+  );
+
+  expect(res.status).toMatchInlineSnapshot(`200`);
+  expect(res.headers.get("content-type")).toMatchInlineSnapshot(
+    `"text/html; charset=utf-8"`
+  );
+  expect(res.headers.get("transfer-encoding")).toMatchInlineSnapshot(
+    `"chunked"`
+  );
+  expect(html).toMatchInlineSnapshot(`"<div>Hello Dylan</div>"`);
+});
+
+test("Globals Template", async () => {
+  const { res, html } = await fetchHtml(
+    express()
+      .use(markoMiddleware())
+      .use((req, res) => {
+        res.locals.greeting = "Goodbye";
+        res.marko(GlobalsTemplate, { $global: { name: "Michael" } });
+      })
+  );
+
+  expect(res.status).toMatchInlineSnapshot(`200`);
+  expect(res.headers.get("content-type")).toMatchInlineSnapshot(
+    `"text/html; charset=utf-8"`
+  );
+  expect(res.headers.get("transfer-encoding")).toMatchInlineSnapshot(
+    `"chunked"`
+  );
+  expect(html).toMatchInlineSnapshot(`"<div>Goodbye Michael</div>"`);
+});
+
 test("Error In Template", async () => {
   const { res, html } = await fetchHtml(
     express()
@@ -37,12 +78,11 @@ test("Error In Template", async () => {
           err: Error,
           req: express.Request,
           res: express.Response,
-          next: unknown
+          next: () => void
         ) => {
-          expect(
-            err.message && err.message.slice(0, err.message.indexOf("\n"))
-          ).toMatchInlineSnapshot(`"Error: fail"`);
+          expect(err.message).toMatchInlineSnapshot(`"fail"`);
           res.status(500).send("Something broke!");
+          next();
         }
       )
   );
@@ -53,11 +93,11 @@ test("Error In Template", async () => {
 
 async function fetchHtml(app: Express) {
   const server = app.listen();
-  await new Promise((resolve) => server.once("listening", resolve));
+  await new Promise(resolve => server.once("listening", resolve));
   const res = await fetch(
     `http://localhost:${(server.address() as AddressInfo).port}`
   );
   const html = await res.text();
-  await new Promise((resolve) => server.close(resolve));
+  await new Promise(resolve => server.close(resolve));
   return { res, html };
 }
